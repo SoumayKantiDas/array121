281246a86aebbd0bf3a5abda6f2201a3a4fb5d56
Soumay kanti Das <anikds2859@outlook.com>
2021-06-08 23:46:04 +0600

    Add project files.

---
+++ array121/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace array121
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            int N;
+            s = Console.ReadLine();
+            N = Convert.ToInt32(s);
+            int[] a = { 1, 2, 3 };
+            for (int i=0; i<a.Length;i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            foreach(int i in a)
+            {
+                Console.WriteLine( "{0}",i);
+            }
+        }
+    }
+}
